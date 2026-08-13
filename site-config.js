86ebeee50befbf87be3a4119035b34c367ed9a53
Russ Caldwell <hello@expatagogo.com>
2026-08-13 09:33:00 -0600

    Show public episode 1 on homepage

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 window.EXPATAGOGO = {
   // Episode 3 is the newest launch episode, so the large homepage player shows it once public.
-  latestYouTubeVideoId: "00QvlkTMgxQ",
+  latestYouTubeVideoId: "DJl8Ls6uDRM",
   episode1YouTubeVideoId: "DJl8Ls6uDRM",
   episode2YouTubeVideoId: "BM8JRAWhadc",
   episode3YouTubeVideoId: "00QvlkTMgxQ"
